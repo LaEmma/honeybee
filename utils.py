d9fc8e7eecc4fed5f53b0dacb08c5927172ff1df
LaEmma <hiliuchao@live.cn>
2018-11-25 22:37:06 +0800

    test

--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def is_valid_data(origin_number, loss_number):
-    # 如果loss >= origin,则为无效数据
+    # 如果loss >= origin, 则为无效数据
     try:
         if int(loss_number) < int(origin_number) and int(origin_number) > 0:
             return True 
